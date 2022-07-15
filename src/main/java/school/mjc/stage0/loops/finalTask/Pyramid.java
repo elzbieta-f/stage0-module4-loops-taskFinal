1911283469b034173d68f675c4352c3ec3230971
elzbieta-f <elzbieta.fulga@gmail.com>
2022-07-15 23:18:13 +0300

    Submission of the rest of the final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j < cathetusLength*2; j++) {
+                if (j>cathetusLength-i&&j<cathetusLength+i) {
+                    if (j<=cathetusLength) {
+                        System.out.print(cathetusLength+1-j);
+                    } else {
+                        System.out.print(j-cathetusLength+1);
+                    }
+                } else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
